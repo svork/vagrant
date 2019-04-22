88e8b2a951f802b84ca4b3135edfb4f13484c740
Rodrigo Costa <svork@users.noreply.github.com>
2019-04-22 17:22:43 -0300

    Remove bootstrap and index.html files

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script install apache and sets the document root to /vagrant
-apt-get update
-apt-get install apache2 -y
-if ! [ -L /var/www/html ]; then
-  rm -rf /var/www/html
-  ln -fs /vagrant /var/www/html
-fi
